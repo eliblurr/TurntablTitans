240b71f5d433c726f4f1f96be719a6fd2bac2f50
Kwame Sarfo <ksarfo69@gmail.com>
2023-04-27 18:14:21 +0000

    refactored model service
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>

--- hackproject/code/api/app/enums.py
+++ hackproject/code/api/app/enums.py
@@ -11,3 +11,63 @@ class Document(Enum):
     INDEPENDENT_CONTRACTOR_AGREEMENT = 'INDEPENDENT_CONTRACTOR_AGREEMENT'
     LOAN_AGREEMENT = 'LOAN_AGREEMENT'
     PARTNERSHIP_AGREEMENT = 'PARTNERSHIP_AGREEMENT'
+
+class Prompts(Enum):
+    SUMMARY = "Can you give me a summary of the document using easy to understand words or non-legal terms"
+    INSURANCE = {
+        "included_in_cover" : "What is included in the cover?",
+        "excluded_from_cover" : "What is excluded from the cover?",
+        "emergency_information" :"Who should I contact in case of emergency?"
+    }
+    LAND = {
+        "description" : "Describe the property briefly?",
+        "terms_of_use" : "What are the terms of use?",
+        "warranties_and_guarantees" : "What are the warranties and guarantees?"
+    }
+    SERVICE_CONTRACT = {
+        "payment_and_services" : "How and what exactly is the vendor being paid for its services?",
+        "obligations" : "What are my obligations?",
+        "liability" : "How will be responsible for mistakes?"
+    }
+    EMPLOYMENT_CONTRACT = {
+        "start_date" : "What is the start date?",
+        "benefits_and_packages" : "What are the benefits and packages?",
+        "schedule" : "Is there a defined schedule?",
+        "response_deadline" :"Am I expected to give my answer before a certain date?"
+    }
+    CONFIDENTIALITY_AGREEMENT = {
+        "confidential_info" : "What constitutes confidential information?",
+        "timeframe" : "Within what timeframe does this agreement hold?",
+        "breach" : "What constitutes a breach?",
+        "obligations" : "What are my obligations?",
+        "steps_upon_violation" : "What steps will be taken if I violate?"
+    }
+    SALES_CONTRACT = {
+        "coverage" : "What are the goods and services covered?",
+        "payment_plan" : "Is there a payment plan in place?",
+        "details_of_delivery" : "What are the details of delivery?"
+    }
+    INDEPENDENT_CONTRACTOR_AGREEMENT = {
+        "schedule" : "Will I be required to work a set schedule?",
+        "payment_terms" : "What are the payment terms?",
+        "details_of_termination" : "What is the minimum notice which will be provided in case of an,"
+                                                     "early contract termination?"
+    }
+    LOAN_AGREEMENT = {
+        "loan_amount" : "What is the loan amount?",
+        "interest_rate" : "What is the interest rate?",
+        "timeframe" : "What is the timeframe of the agreement?",
+        "method_of_repayment" : "What is the method of repayment?",
+        "late_payment_info" :"What happens in case there are late or missed payments?"
+    }
+    PARTNERSHIP_AGREEMENT = {
+        "responsibilities" : "What are the responsibilities of the partners?",
+        "restrictions" : "What are the restriction on partners",
+        "dispute_resolution" : "What will disputes be resolved"
+    }
+
+    @classmethod
+    def value_of(cls, value):
+        for k, v in cls.__members__.items():
+            if k == value:
+                return v
--- hackproject/code/api/app/services/model_service/model_service.py
+++ hackproject/code/api/app/services/model_service/model_service.py
@@ -3,7 +3,7 @@ from abc import abstractmethod
 from typing import List
 
 from llama_index import GPTSimpleVectorIndex
-from hackproject.code.api.app.enums import Document
+from hackproject.code.api.app.enums import Document, Prompts
 from hackproject.code.api.app.schemas.model_service.model_service_schemas import InsuranceDocumentResponseSchema, \
     LandDocumentResponseSchema, ServiceContractDocumentResponseSchema, EmploymentContractDocumentResponseSchema, \
     ConfidentialityAgreementDocumentResponseSchema, SalesContractDocumentResponseSchema, \
@@ -50,109 +50,61 @@ class ModelServiceImpl(ModelService):
 
 
     def __generate_summary(self):
-        return self.process_prompt("Can you give me a summary of the document using easy to understand words or"
-                                   " non-legal terms")
+        return self.process_prompt(Prompts.SUMMARY.value)
 
     def process_prompt(self, prompt: str):
         return self.__index.query(prompt).response
 
     def __process_insurance_document(self):
         summary = self.__generate_summary()
-        included_in_cover = self.process_prompt("What is included in the cover?")
-        excluded_from_cover = self.process_prompt("What is excluded from the cover?")
-        emergency_information = self.process_prompt("Who should I contact in case of emergency?")
-        return InsuranceDocumentResponseSchema(summary=summary,
-                                               included_in_cover=included_in_cover,
-                                               excluded_from_cover=excluded_from_cover,
-                                               emergency_information=emergency_information)
+        prompts: dict = Prompts.value_of(Document.INSURANCE.value).value
+        answers = {k: self.process_prompt(v) for k,v in prompts.items()}
+        return InsuranceDocumentResponseSchema(summary=summary, **answers)
 
     def __process_land_document(self):
         summary = self.__generate_summary()
-        description = self.process_prompt("Describe the property briefly")
-        terms_of_use = self.process_prompt("What are the terms of use?")
-        warranties_and_guarantees = self.process_prompt("What are the warranties and guarantees")
-        return LandDocumentResponseSchema(summary=summary,
-                                          description=description,
-                                          terms_of_use=terms_of_use,
-                                          warranties_and_guarantees=warranties_and_guarantees)
+        prompts: dict = Prompts.value_of(Document.LAND.value).value
+        answers = {k: self.process_prompt(v) for k, v in prompts.items()}
+        return LandDocumentResponseSchema(summary=summary, **answers)
 
     def __process_service_contract_document(self):
         summary = self.__generate_summary()
-        payment_and_services = self.process_prompt("How and what exactly is the vendor being paid for its services?")
-        obligations = self.process_prompt("What are my obligations?")
-        liability = self.process_prompt("How will be responsible for mistakes?")
-        return ServiceContractDocumentResponseSchema(summary=summary,
-                                                     payment_and_services=payment_and_services,
-                                                     obligations=obligations,
-                                                     liability=liability)
+        prompts: dict = Prompts.value_of(Document.SERVICE_CONTRACT.value).value
+        answers = {k: self.process_prompt(v) for k, v in prompts.items()}
+        return ServiceContractDocumentResponseSchema(summary=summary, **answers)
 
     def __process_employment_contract_document(self):
         summary = self.__generate_summary()
-        start_date = self.process_prompt("What is the start date?")
-        benefits_and_packages = self.process_prompt("What are the benefits and packages")
-        schedule = self.process_prompt("Is there a defined schedule?")
-        response_deadline = self.process_prompt("Am I expected to give my answer before a certain date?")
-        return EmploymentContractDocumentResponseSchema(summary=summary,
-                                                        start_date=start_date,
-                                                        benefits_and_packages=benefits_and_packages,
-                                                        schedule=schedule,
-                                                        response_deadline=response_deadline)
+        prompts: dict = Prompts.value_of(Document.EMPLOYMENT_CONTRACT.value).value
+        answers = {k: self.process_prompt(v) for k, v in prompts.items()}
+        return EmploymentContractDocumentResponseSchema(summary=summary, **answers)
 
     def __process_confidentiality_agreement_document(self):
         summary = self.__generate_summary()
-        confidential_info = self.process_prompt("What constitutes confidential information?")
-        timeframe = self.process_prompt("Within what timeframe does this agreement hold?")
-        breach = self.process_prompt("What constitutes a breach?")
-        obligations = self.process_prompt("What are my obligations?")
-        steps_upon_violation = self.process_prompt("What steps will be taken if I violate")
-        return ConfidentialityAgreementDocumentResponseSchema(summary=summary,
-                                                              confidential_info=confidential_info,
-                                                              timeframe=timeframe,
-                                                              breach=breach,
-                                                              obligations=obligations,
-                                                              steps_upon_violation=steps_upon_violation)
+        prompts: dict = Prompts.value_of(Document.CONFIDENTIALITY_AGREEMENT.value).value
+        answers = {k: self.process_prompt(v) for k, v in prompts.items()}
+        return ConfidentialityAgreementDocumentResponseSchema(summary=summary, **answers)
 
     def __process_sales_contract_document(self):
         summary = self.__generate_summary()
-        coverage = self.process_prompt("What are the goods and services covered?")
-        payment_plan = self.process_prompt("Is there a payment plan in place?")
-        details_of_delivery = self.process_prompt("What are the details of delivery?")
-        return SalesContractDocumentResponseSchema(summary=summary,
-                                                   coverage=coverage,
-                                                   payment_plan=payment_plan,
-                                                   details_of_delivery=details_of_delivery)
+        prompts: dict = Prompts.value_of(Document.SALES_CONTRACT.value).value
+        answers = {k: self.process_prompt(v) for k, v in prompts.items()}
+        return SalesContractDocumentResponseSchema(summary=summary, **answers)
 
     def __process_independent_contractor_agreement_document(self):
         summary = self.__generate_summary()
-        schedule = self.process_prompt("Will I be required to work a set schedule?")
-        payment_terms = self.process_prompt("What are the payment terms?")
-        details_of_termination = self.process_prompt("What is the minimum notice which will be provided in case of an,"
-                                                  "early contract termination?")
-        return IndependentContractorAgreementDocumentResponseSchema(summary=summary,
-                                                                    schedule=schedule,
-                                                                    payment_terms=payment_terms,
-                                                                    details_of_termination=details_of_termination)
+        prompts: dict = Prompts.value_of(Document.INDEPENDENT_CONTRACTOR_AGREEMENT.value).value
+        answers = {k: self.process_prompt(v) for k, v in prompts.items()}
+        return IndependentContractorAgreementDocumentResponseSchema(summary=summary, **answers)
 
     def __process_loan_agreement_document(self):
         summary = self.__generate_summary()
-        loan_amount = self.process_prompt("What is the loan amount?")
-        interest_rate = self.process_prompt("What is the interest rate?")
-        timeframe = self.process_prompt("What is the timeframe of the agreement?")
-        method_of_repayment = self.process_prompt("What is the method of repayment?")
-        late_payment_info = self.process_prompt("What happens in case there are late or missed payments?")
-        return LoanAgreementDocumentResponseSchema(summary=summary,
-                                                   loan_amount=loan_amount,
-                                                   interest_rate=interest_rate,
-                                                   timeframe=timeframe,
-                                                   method_of_repayment=method_of_repayment,
-                                                   late_payment_info=late_payment_info)
+        prompts: dict = Prompts.value_of(Document.LOAN_AGREEMENT.value).value
+        answers = {k: self.process_prompt(v) for k, v in prompts.items()}
+        return LoanAgreementDocumentResponseSchema(summary=summary, **answers)
 
     def __process_partnership_agreement_document(self):
         summary = self.__generate_summary()
-        responsibilities = self.process_prompt("What are the responsibilities of the partners?")
-        restrictions = self.process_prompt("What are the restriction on partners")
-        dispute_resolution = self.process_prompt("What will disputes be resolved")
-        return PartnershipAgreementDocumentResponseSchema(summary=summary,
-                                                          responsibilities=responsibilities,
-                                                          restrictions=restrictions,
-                                                          dispute_resolution=dispute_resolution)
+        prompts: dict = Prompts.value_of(Document.PARTNERSHIP_AGREEMENT.value).value
+        answers = {k: self.process_prompt(v) for k, v in prompts.items()}
+        return PartnershipAgreementDocumentResponseSchema(summary=summary, **answers)
