656e376fa77028d8692d5d7ac51b7ff8efdbb3dd
Afsanat Ineza <afsanat.ineza@gmail.com>
2023-05-05 04:08:12 +0000

    dyslexia theme

---
+++ hackproject/code/frontend/src/app/theming/theme.ts
@@ -0,0 +1,35 @@
+export const THEMES:any = {
+    dyslexia: {
+      primaryColor: 'var(--primary)',
+      secondaryColor: 'var(--secondary)',
+      tertiaryColor: 'var(--tertiary)',
+      textOnTertiary: 'var(--textOnTertiary)',
+      textOnPrimary: 'var(--textLight)',
+      textOnSecondary: 'var(--dm-textLight)',
+      background: ' var(--background)',
+    },
+    dark: {
+      primaryColor: 'hsl(168deg 100% 29%)',
+      secondaryColor: 'hsl(161deg 94% 13%)',
+      textOnPrimary: 'hsl(0, 0%, 100%)',
+      tertiaryColor: 'hsl(0, 0%, 100%)',
+      textOnTertiary: 'hsl(0, 0%, 100%)',
+      textOnSecondary: 'hsl(0, 0%, 100%)',
+      background: 'hsl(0, 0%, 10%)',
+    },
+    netflix: {
+      primaryColor: 'hsl(357, 92%, 47%)',
+      secondaryColor: 'hsl(0, 0%, 8%)',
+      textOnPrimary: 'hsl(0, 0%, 100%)',
+      textOnSecondary: 'hsl(0, 0%, 100%)',
+      background: 'hsl(0deg 0% 33%)',
+    },
+    spotify: {
+      primaryColor: 'hsl(132, 65%, 55%)',
+      secondaryColor: 'hsl(0, 0%, 0%)',
+      textOnPrimary: 'hsl(229, 61%, 42%)',
+      textOnSecondary: 'hsl(0, 0%, 100%)',
+      background: 'hsl(0, 0%, 100%)',
+    },
+  };
+  
