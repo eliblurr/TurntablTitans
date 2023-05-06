2427b50a370580f12d94beb0b365af01525ffe8c
Judith Ofosu <61045983+jsofosu@users.noreply.github.com>
2023-05-06 09:08:57 +0000

    Dev (#37)
    * Accessibility Themes Variations (#34)
    
    * Create angular.yml
    
    * Create fastAPI.yml
    
    * Update angular.yml
    
    * Update angular.yml
    
    * Update fastAPI.yml
    
    * Feature/model service (#1)
    
    created model service
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * Feature/text translation (#2)
    
    * added text translation
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * Update fastAPI.yml
    
    * Update fastAPI.yml
    
    * Feature/text translation (#6)
    
    * refactoring requirements.txt
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * Update fastAPI.yml
    
    * Update fastAPI.yml
    
    * Update fastAPI.yml
    
    * Update fastAPI.yml
    
    * Feature/text translation (#11)
    
    * refactored tests
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * Feature/messaging (#12)
    
    * added open source embedding model
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * Feature/messaging (#12)
    
    * added open source embedding model
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * add ui components
    
    * Test/model service (#13)
    
    * added model service tests
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * tts setup
    
    * ui tweaks
    
    * tts setup
    
    * Update fastAPI.yml
    
    * resolve package not found
    
    * theme switch logic
    
    * dyslexia theme
    
    * All themes
    
    * motion sensitivity theme
    
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    Co-authored-by: eliblurr <elvissegbawu@gmail.com>
    Co-authored-by: Kwame Adu-Poku Sarfo <73546735+Ksarfo69@users.noreply.github.com>
    Co-authored-by: Judith Ofosu <61045983+jsofosu@users.noreply.github.com>
    
    * add file upload service
    
    * wip
    
    Signed-off-by: Judith Ofosu <61045983+jsofosu@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    Signed-off-by: Judith Ofosu <61045983+jsofosu@users.noreply.github.com>
    Co-authored-by: afsanat <105736732+afsanat@users.noreply.github.com>
    Co-authored-by: eliblurr <elvissegbawu@gmail.com>
    Co-authored-by: Kwame Adu-Poku Sarfo <73546735+Ksarfo69@users.noreply.github.com>

--- hackproject/code/frontend/src/app/features/file-upload/file-upload.component.ts
+++ hackproject/code/frontend/src/app/features/file-upload/file-upload.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MatDialogRef} from "@angular/material/dialog";
 import {FileUploadService} from "../../shared/services/file-upload/file-upload.service";
+import {File, FileUploadRequest} from "../../shared/models/file-upload";
+
 
 @Component({
   selector: 'app-file-upload',
@@ -17,24 +19,33 @@ export class FileUploadComponent {
   ) {
   }
 
-  uploadFileForm: FormGroup = this.formBuilder.group({
-    file: this.formBuilder.control('', Validators.required),
-    category: this.formBuilder.control(null, Validators.required),
-    language: this.formBuilder.control('', Validators.required)
-  })
-  categories: string [] = [
-    'Insurance', 'Land'
-  ]
 
-  languages: string[] = [
-    'English', 'French'
-  ]
+  ngOnInit(): void {
+    this.getFileTypes()
+    this.getLanguages()
+  }
+
+  uploadFileForm: FormGroup = this.formBuilder.group({
+    type: this.formBuilder.control('', Validators.required),
+    doc_language: this.formBuilder.control('', Validators.required),
+    file_path: this.formBuilder.control('', Validators.required),
+    native_language: this.formBuilder.control('English', Validators.required)
+  })
+  categories: string [] = []
+
+  languages: string[] = []
+
 
   cancel() {
     this.dialog.close()
   }
 
   uploadFile() {
+    const chatId = localStorage.getItem('chat_id')
+    if(!chatId) return
+    const request: FileUploadRequest = {prompt: this.uploadFileForm.value, chat_id:chatId}
+    this.fileService.uploadDocument(request)
+
   console.log(this.uploadFileForm.value)
   }
 
