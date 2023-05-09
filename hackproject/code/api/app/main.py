8b4ca96b5c840b8874ad23536322680c0c99cde8
Kwame Adu-Poku Sarfo <73546735+Ksarfo69@users.noreply.github.com>
2023-05-09 17:22:48 +0000

    Documentation (#40)
    * added usecases
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * fetched upstream
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * fetched upstream
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * upload file and display response
    
    * added support for .docx
    
    * added docx2txt dependency
    
    * added approach image
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * chat feature
    
    * added file upload support
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * added file upload functionality
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * added to usecase story
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * more progress on usecase documentation
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * more progress on usecase documentation
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * initial use case
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * cleaning up use case scenario
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * cleaning up readme
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * cleaning up readme
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * cleaning up business decision documentation
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * added project README
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * more documentation
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * refactoring
    
    * added new replies
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * added new replies
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * added arch diagram
    
    * refactoring
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * cleaned up responses
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    * fixed some bugs in response
    
    * caught exceptions
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    
    ---------
    
    Signed-off-by: Kwame Sarfo <ksarfo69@gmail.com>
    Co-authored-by: Judith Ofosu <61045983+jsofosu@users.noreply.github.com>

--- hackproject/code/api/app/main.py
+++ hackproject/code/api/app/main.py
@@ -112,7 +112,7 @@ async def file(type: str = Form(...),
         os.remove(file_path)
         return response
     except Exception:
-        return {"message": "Oops, an error occurred. Let's try that again."}
+        raise HTTPException(status_code=500, detail="An error occurred, please try again.")
 
 @router.post("/stt")
 async def stt_(
@@ -129,24 +129,44 @@ async def stt_(
 
 @router.post("/tts/{message_id}")
 async def tts(message_id:str, payload:TTS):
-    path = parrot(payload.text, payload.language, message_id)
-    return FileResponse(path, background=BackgroundTask(remove_file, path))
+    try:
+        path = parrot(payload.text, payload.language, message_id)
+        return FileResponse(path, background=BackgroundTask(remove_file, path))
+    except:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="An error occurred, please try again.")
 
 @router.get("/axa/products", response_model=list[AXAProduct])
 async def get_products():
-    return await products()
+    try:
+        return await products()
+    except:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="An error occurred, please try again.")
 
 @router.get("/axa/{product}/questions", response_model=list[Question])
 async def questions(product:str, language: str = 'en'):
-    return await get_questions(product, language)
+    try:
+        return await get_questions(product, language)
+    except:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="An error occurred, please try again.")
 
 @router.get("/axa/{product}/questions/{question_id}", response_model=QuestionDetail)
 async def get_question_by_id(product:str, question_id:str, language: str = 'en'):
-    return await get_questions(product, language, question_id)
+    try:
+        return await get_questions(product, language, question_id)
+    except:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="An error occurred, please try again.")
 
 @router.post("/axa/{product}/compute", response_model=SubmissionResponse)
 async def submit(product:str, payload:list[State], language: str = 'en'):
-    return await compute(product, language, payload)
+    try:
+        return await compute(product, language, payload)
+    except:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="An error occurred, please try again.")
 
 @router.get("/chat/web")
 async def web_prompt():
@@ -161,6 +181,7 @@ async def web_prompt(request: WebPrompt):
         return prompt_response
     except:
         traceback.print_exc()
+        raise HTTPException(status_code=500, detail="An error occurred, please try again.")
 
 @router.post("/chat/messaging/" + bot.token)
 async def mobile_prompt(request: Request):
