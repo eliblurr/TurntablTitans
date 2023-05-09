eea7d260c3207aa4698a54ae45caa5f9b9bc3ea3
Judith Ofosu <61045983+jsofosu@users.noreply.github.com>
2023-05-09 11:24:32 +0000

    Chats session management (#57)
    * upload file and display response
    
    * chat feature
    
    * ui tweaks

--- hackproject/code/frontend/src/app/shared/services/shared/shared.service.ts
+++ hackproject/code/frontend/src/app/shared/services/shared/shared.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Chat, Message} from "../../models/chat";
 import {SynthesisService} from "../../services/text-speech-synth/synthesis.service"
-import {HttpClient} from "@angular/common/http";
-import { Observable, catchError, map, of } from 'rxjs';
+import {BehaviorSubject} from "rxjs";
 
 
 @Injectable({
@@ -13,15 +12,14 @@ export class SharedService {
   chatId!: string
   loading = false
   nativeLanguage: string = 'ENGLISH'
+  sessionMessages = new BehaviorSubject<Chat[]>([])
+  $chats = this.sessionMessages.asObservable();
   messages: Message[] = []
-  chats: Chat[] = []
   chatMap: Map<string, Message[]> = new Map<string, Message[]>();
   file!: File
 
   constructor(private synthesisService: SynthesisService) {
     this.synthesisService = synthesisService
-    // this.http = http
-    // , private http: HttpClient
   }
 
   startNewChat() {
@@ -64,8 +62,13 @@ export class SharedService {
 
   addToChatsList(chatId: string, chatName: string) {
     const newChat = {chatId: chatId, chatName: chatName}
-    this.chats.push(newChat)
-    localStorage.setItem('chatsList', JSON.stringify(this.chats))
+    if (this.getChatsListFromLocalStorage().length !== 0) {
+      this.sessionMessages.next( this.getChatsListFromLocalStorage())
+    }
+    this.sessionMessages.next([...this.sessionMessages.value, newChat])
+    this.$chats.subscribe((res) => {
+      localStorage.setItem('chatsList', JSON.stringify(res))
+    })
   }
 
   // Save the chat data to local storage
@@ -75,6 +78,10 @@ export class SharedService {
 
   // Retrieve the messages for a specific chat ID
   getMessages(chatId: string): Message[] | undefined {
+    const storedData = localStorage.getItem('chatData');
+    if (storedData) {
+      this.chatMap = new Map<string, Message[]>(JSON.parse(storedData));
+    }
     return this.chatMap.get(chatId);
   }
 
@@ -86,42 +93,51 @@ export class SharedService {
     }
   }
 
-  hashText(text:string){
+  getChatsListFromLocalStorage(): Chat[] {
+    const chatsListString = localStorage.getItem('chatsList');
+    if (chatsListString) {
+      return JSON.parse(chatsListString);
+    } else {
+      return [];
+    }
+  }
+
+  hashText(text: string) {
     var hash = 0, i, chr;
     if (text.length === 0) return hash;
     for (i = 0; i < text.length; i++) {
-      chr   = text.charCodeAt(i);
-      hash  = ((hash << 5) - hash) + chr;
-      hash |= 0; // Convert to 32bit integer     
+      chr = text.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
     }
-    const res = hash.toString() 
-    return hash>0 ? res :  res.replace("-", "1")
+    const res = hash.toString()
+    return hash > 0 ? res : res.replace("-", "1")
   }
 
-  playAudio(blobUrl:string){
+  playAudio(blobUrl: string) {
     let audio = new Audio();
     audio.src = blobUrl
     audio.load();
     audio.play();
   }
 
-  async sourceFileExists(url: string){
+  async sourceFileExists(url: string) {
     var exists = true
-    await fetch(url).then(res=> exists=true ).catch(err=> exists=false )
+    await fetch(url).then(res => exists = true).catch(err => exists = false)
     return exists
-  } 
+  }
 
-  async textToSpeech(text:string, language:string) {
+  async textToSpeech(text: string, language: string) {
     const hash = this.hashText(text)
     var blobUrl = localStorage.getItem('audio_blob') || ''
 
-    if (hash==localStorage.getItem('audio_hash') && await this.sourceFileExists(blobUrl)){  
+    if (hash == localStorage.getItem('audio_hash') && await this.sourceFileExists(blobUrl)) {
       return this.playAudio(blobUrl);
     }
 
-    return this.synthesisService.fetch_audio({"text":text,"language":language}, hash.toString())
+    return this.synthesisService.fetch_audio({"text": text, "language": language}, hash.toString())
       .subscribe(
-        (blob:any)=> {
+        (blob: any) => {
           var blobUrl = URL.createObjectURL(blob);
           this.playAudio(blobUrl)
           // load blob and hash into memory
