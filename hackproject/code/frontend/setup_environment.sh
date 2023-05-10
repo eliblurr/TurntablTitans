f99ba11d12eded80fe8b7003b697da92423e3f1f
eliblurr <elvissegbawu@gmail.com>
2023-05-10 21:35:22 +0000

    script

--- hackproject/code/frontend/setup_environment.sh
+++ hackproject/code/frontend/setup_environment.sh
@@ -1,3 +1,21 @@
-tmp=$(mktemp)
-jq '."/api/v1".target = env.API_BASE_URL' ./proxy.conf.json > "$tmp" && mv "$tmp" ./proxy.conf.json
-# npm run build
+#!/bin/bash
+# pass in api url from shell
+# ./setup_environ.sh -c url
+# -c is required
+
+while getopts ":c:" opt; do
+  case $opt in
+    c) cmd="$OPTARG";;
+  esac
+done
+
+if [ -z "$cmd" ]
+then
+  echo '-c is required, add api url'
+else
+  echo 'url accepted'
+  export API_BASE_URL=${cmd}
+  echo "apiUrl->$API_BASE_URL"
+  tmp=$(mktemp)
+  jq '."/api/v1".target = env.API_BASE_URL' ./proxy.conf.json > "$tmp" && mv "$tmp" ./proxy.conf.json
+fi && exit 1
